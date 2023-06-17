f1b603258b334f2f3b5949cc7ee749b917390c44
Humam Samra <Humamdaraya@gmail.com>
2023-06-17 23:40:37 +0300

    language

--- assets/dist/js/init.js
+++ assets/dist/js/init.js
@@ -2,9 +2,21 @@ const appSettings = {
     name: "Coffee Shop",
 };
 
+const appSettingsAR = {
+    name: "Coffee Shop",
+};
+
 function init() {
     document.querySelectorAll('[appData]').forEach(element => {
-        element.innerHTML = appSettings[element.getAttribute('appData')]
+        if(localStorage.getItem('language')){
+            if(localStorage.getItem('language') == 'ar'){
+                element.innerHTML = appSettingsAR[element.getAttribute('appData')] ?? element.getAttribute('appData');
+            }else{
+                element.innerHTML = appSettings[element.getAttribute('appData')] ?? element.getAttribute('appData');
+            }
+        }else{
+            element.innerHTML = appSettings[element.getAttribute('appData')] ?? element.getAttribute('appData');
+        }
     }); 
 }
 document.addEventListener('DOMContentLoaded', init);
