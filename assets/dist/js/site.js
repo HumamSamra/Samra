89e29808eae50ce58eff3799fc66063ca409eda1
Humam Samra <Humamdaraya@gmail.com>
2023-06-17 22:36:40 +0300

    header

---
+++ assets/dist/js/site.js
@@ -0,0 +1,10 @@
+(function ($){
+    "use strict";
+    
+    $(window).on('load', function () {
+        $('.loaderUp').addClass('done');
+        $('.loaderDown').addClass('done');
+        $('main').addClass("blurIn");
+    });
+
+})(jQuery);
