39846a4b9c2191da16ae6bd05982b446d2eebac4
Humam Samra <Humamdaraya@gmail.com>
2023-06-18 14:03:34 +0300

    icon

--- assets/dist/js/site.js
+++ assets/dist/js/site.js
@@ -4,7 +4,6 @@
     $(window).on('load', function () {
         $('.loaderUp').addClass('done');
         $('.loaderDown').addClass('done');
-        $('main').addClass("blurIn");
     });
 
 })(jQuery);
