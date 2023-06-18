98ae69372103b26d9ce89cdf1010584f59921ea9
Humam Samra <Humamdaraya@gmail.com>
2023-06-18 13:17:55 +0300

    language tr

--- assets/dist/js/init.js
+++ assets/dist/js/init.js
@@ -1,27 +1,39 @@
 const appSettings = {
     name: "Coffee Shop",
     world: "Hello World",
-    samra: "Samra"
+    samra: "Samra",
+    home: "Home",
+    about: "About us",
+    tr: "Turkish",
+    en: "English",
+    lang: "Language",
+    search: "Search"
 };
 
-const appSettingsAR = {
-    name: "سوق القهوة",
-    world: "مرحباً بالعالم",
-    samra: "سمرة"
+const appSettingsTR = {
+    name: "Cahve market",
+    world: "dunya",
+    samra: "Samra",
+    home: "ana sayfa",
+    about: "hakkimiz",
+    tr: "Turkish",
+    en: "English",
+    lang: "Dil",
+    search: "Ara"
 };
 
 function init() {
     document.querySelectorAll('[appData]').forEach(element => {
         if(localStorage.getItem('language')){
-            if(localStorage.getItem('language') == 'ar'){
-                element.innerHTML = appSettingsAR[element.getAttribute('appData').toLowerCase()] ?? element.getAttribute('appData');
+            if(localStorage.getItem('language') == 'tr'){
+                element.innerHTML = appSettingsTR[element.getAttribute('appData').toLowerCase()] ?? element.getAttribute('appData');
             }else{
                 element.innerHTML = appSettings[element.getAttribute('appData').toLowerCase()] ?? element.getAttribute('appData');
             }
         }else{
             element.innerHTML = appSettings[element.getAttribute('appData').toLowerCase()] ?? element.getAttribute('appData');
         }
-    }); 
+    });
 }
 document.addEventListener('DOMContentLoaded', init);
 
